7e93e64d27a3a250d7fa1c8840ca15b2a5ba0c17
Akshaypratapsingh <2k24.cs1c.2414104@gmail.com>
2025-10-12 20:46:35 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>jai mata di</h1>
+      <h1 className="bg-red-500 text-white p-4">jai mata di</h1>
     </div>
   );
 };
